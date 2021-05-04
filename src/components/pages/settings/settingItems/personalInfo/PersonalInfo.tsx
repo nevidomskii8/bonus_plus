527cabec36716867604c877e4b74f2ce11f42511
nevidomskii8 <49536061+nevidomskii8@users.noreply.github.com>
2021-05-04 10:48:07 +0300

    resolve import css-modules in typeScript file

--- src/components/pages/settings/settingItems/personalInfo/PersonalInfo.tsx
+++ src/components/pages/settings/settingItems/personalInfo/PersonalInfo.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import styles from "./PersonalInfo.module.scss";
+import styles from "./personalInfo.module.scss";
 
 export const PersonalInfo = () => {
+
+  const className: string = styles.PersonalInfo
+
   return (
-    <div className={styles.PersonalInfo}>
+    <div className={className}>
       <h2>Персональные данные (335588)</h2>
 
       <h3>Введите ваши персональные данные и выбирте место вашего проживания</h3>
